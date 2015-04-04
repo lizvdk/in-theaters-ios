81245fa365d7f09a6f4ee991d88315a023b9b250
Liz Vanderkloot <liz@barbershoplabs.com>
2015-04-04 11:48:23 -0400

    RT API call; List movie titles in table
    RT API key is stored in an uncommitted 'notes' file. I will work on
    implementing an ENV mgmt solution in the future. For now, copy in from
    notes; remove before committing.

---
+++ app/api/rt.rb
@@ -0,0 +1,14 @@
+class RT
+  API_KEY = '' # Withheld from commit
+  NOW_URL = "http://api.rottentomatoes.com/api/public/v1.0/lists/movies/in_theaters.json?apikey=#{API_KEY}"
+
+  def now(&callback)
+    AFMotion::JSON.get(NOW_URL) do |result|
+      if result.success?
+        callback.call result.object
+      else
+        callback.call nil
+      end
+    end
+  end
+end
--- app/screens/movies_screen.rb
+++ app/screens/movies_screen.rb
@@ -2,6 +2,19 @@ class MoviesScreen < PM::TableScreen
   title "Now Playing"
 
   def table_data
-    []
+      [{
+        cells: Array(@movies)
+      }]
+  end
+
+  def on_load
+    RT.new.now do |response|
+      @movies = response["movies"].map do |movie|
+        {
+          title: movie["title"],
+        }
+      end
+      update_table_data
+    end
   end
 end
